8c7280545a532b48acf2e9f6f6b954d0e52ae210
punojbista55@gmail.com <dipen55555>
2021-11-14 22:53:04 +0545

    Making Stack class Generic and reverse string using Stack!!

--- src/com/punoj/ds/stack/Stack.java
+++ src/com/punoj/ds/stack/Stack.java
@@ -1,41 +1,45 @@
 package com.punoj.ds.stack;
 
-public class Stack {
-    private double container[] = {};
+import java.lang.reflect.Array;
+
+public class Stack<T> {
+    private T stackContainer[];
     //not removing actual data in array just changing pointer
     private int top;
     private int maxSize;
 
-    public Stack(int size) {
-        this.container = new double[size];
+    public Stack(Class<T> c, int size) {
+        // Use Array native method to create array
+        // of a type only known at run time
+        @SuppressWarnings("unchecked") final T[] a = (T[]) Array.newInstance(c, size);
+        this.stackContainer = a;
         top = -1;
         maxSize = size;
-
     }
 
-    public void push(double input) {
+    public void push(T input) {
         if (!isFull()) {
             top++;
-            container[top] = input;
+            stackContainer[top] = input;
         } else {
 
             System.out.println("Stack is Full");
         }
     }
 
-    public double pop() {
+    public T pop() {
         if (!isEmpty()) {
             int old_top = top;
             top--;
-            return container[old_top];
+            return stackContainer[old_top];
         } else {
             System.out.println("Stack is already empty !!");
-            return -1;
         }
+        return null;
     }
 
-    public double peek() {
-        return container[top];
+    public T peek() {
+        return stackContainer[top];
     }
 
     public boolean isEmpty() {
